4c126dfcaa36d3a3900ae045a10197117b3733a8
Anarkila <57869565+anarkila@users.noreply.github.com>
2021-12-10 19:08:20 +0200

    added private set

--- Console/Assets/DeveloperConsole/Scripts/ConsoleCommandData.cs
+++ Console/Assets/DeveloperConsole/Scripts/ConsoleCommandData.cs
@@ -6,22 +6,22 @@ namespace Anarkila.DeveloperConsole {
 
     public class ConsoleCommandData {
 
-        public MonoBehaviour monoScript;
-        public string methodName;
-        public string commandName;
-        public string commandNameLower;
-        public string defaultValue = "";
-        public string info = "";
-        public Type parameterType;
-        public bool isStaticMethod = false;
-        public MethodInfo methodInfo;
-        public bool isCoroutine= false;
-        public bool optionalParameter = false;
-        public bool hiddenCommand = false;
-        public bool hiddenCommandMinimalGUI = false;
-        public string scriptNameString;
-        public char[] commandNameCharArray;
-        public char[] commandNameLowerCharArray;
+        public MonoBehaviour monoScript { get; private set; }
+        public string methodName { get; private set; }
+        public string commandName { get; private set; }
+        public string commandNameLower { get; private set; }
+        public string defaultValue { get; private set; }
+        public string info { get; private set; }
+        public Type parameterType { get; private set; }
+        public bool isStaticMethod { get; private set; }
+        public MethodInfo methodInfo { get; private set; }
+        public bool isCoroutine { get; private set; }
+        public bool optionalParameter { get; private set; }
+        public bool hiddenCommand { get; private set; }
+        public bool hiddenCommandMinimalGUI { get; private set; }
+        public string scriptNameString { get; private set; }
+        public char[] commandNameCharArray { get; private set; }
+        public char[] commandNameLowerCharArray { get; private set; }
 
         public ConsoleCommandData(MonoBehaviour scriptName, string methodName, string commandName, string commandExplanation, string info, Type parameterType,
             bool isStaticMethod, MethodInfo methodInfo, bool isCoroutine, bool optionalParameter, bool hiddenCommand = false,
